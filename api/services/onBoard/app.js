a7e6a534a349999dc4365ac910d8e1ee18ce7b75
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2022-03-27 21:21:30 +0530

    feat added

---
+++ api/services/onBoard/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
+import http from 'http';
+import mongoose from "mongoose";
+
+
+/**
+ * 
+ */
+(async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        });
+        console.log("Database connected successfully! Starting Server!");
+        const app = express();
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        app.use(cors());
+        const server = http.createServer(app);
+        
+        const port = process.env.PORT || 8000;
+        server
+          .listen(port)
+          .on("listening", () => console.log(`App started for onboard on port: ${port}`))
+          .on("error", (err) =>
+            console.log(`An error occured while starting server`, err)
+          );
+    } catch (error) {
+        console.log("Unable to connect with database----->")
+        console.log('error', error)
+        process.exit(1)
+    }
+})();
